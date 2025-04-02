a9373d0f0ae244358a0a3e774c6c9fc148f2c088
GeovanniMarrocos <geovanni.marrocos@gmail.com>
2025-04-02 14:14:35 -0300

    feat: countries-php

---
+++ scripts/404.php
@@ -0,0 +1,13 @@
+<?php 
+defined('CONTROL') or  die('Acesso Inválido');
+?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col text-center">
+            <h3 class="text-danger">Error 404 <br>Recurso não encontrado</h3>
+        </div>
+    </div>
+</div>
+
+
